73c6652f0d7bf33bce6f2354c12c207d7707009a
Joe Reiff <jcreiff@gmail.com>
2015-06-08 15:37:06 -0400

    create 2 seeds for each course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+breakfast = Course.create!(name: "Breakfast")
+lunch = Course.create!(name: "Lunch")
+dinner = Course.create!(name: "Dinner")
+sides = Course.create!(name: "Sides")
+beverages = Course.create!(name: "Beverages")
+desserts = Course.create!(name: "Desserts")
+
+
+Dish.create!(course: breakfast, name: "Eggs", description: "Scrambled, Fried, or Poached", price: 3.99)
+Dish.create!(course: breakfast, name: "Pancakes", description: "Chocolate Chip or Blueberry", price: 7.99)
+
+Dish.create!(course: lunch, name: "Sandwich", description: "BLT, Grilled Cheese, or Turkey Melt", price: 8.99)
+Dish.create!(course: lunch, name: "Soup", description: "Soup of the Day: Cup or Bowl", price: 6.99)
+
+Dish.create!(course: dinner, name: "Steak", description: "Rare, Medium, or Burned", price: 12.99)
+Dish.create!(course: dinner, name: "Pasta", description: "Red Sauce or White Sauce", price: 10.99)
+
+Dish.create!(course: sides, name: "Salad", description: "Ranch or Caesar Dressing", price: 4.99)
+Dish.create!(course: sides, name: "Fries", description: "Curly or Not Curly", price: 3.99)
+
+Dish.create!(course: beverages, name: "Iced Tea", price: 1.99)
+Dish.create!(course: beverages, name: "Coke", price: 1.99)
+
+Dish.create!(course: desserts, name: "Ice Cream", price: 3.99)
+Dish.create!(course: desserts, name: "Pie", price: 3.99)

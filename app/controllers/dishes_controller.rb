586c6232fd05ca3aa6c6569aa2de1746ea180eaa
Joe Reiff <jcreiff@gmail.com>
2015-06-08 16:01:23 -0400

    create very basic index view, _form, and new/edit

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,10 +1,13 @@
 class DishesController < ApplicationController
   def index
+    @courses = Course.all
   end
 
   def new
+    @dish = Dish.new
   end
 
   def edit
+    @dish = Dish.first
   end
 end
